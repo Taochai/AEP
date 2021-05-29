0867b925ca69cc3a7f3f84aa8b4b86aec8339868
tao.chai <tao.chai@thoughtworks.com>
2021-05-29 10:39:51 +0800

    [Tao & Yongqiang] generate receipt when save bag successfully

--- src/main/java/Locker.java
+++ src/main/java/Locker.java
@@ -1,5 +1,13 @@
 public class Locker {
+//    private final int capacity;
+//    private int availableCapacity;
+//
+//    public Locker(int capacity){
+//        this.capacity = capacity;
+//        this.availableCapacity = capacity;
+//    }
+
     public Receipt save(Bag bag) {
-        return null;
+        return new Receipt();
     }
 }
