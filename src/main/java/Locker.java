bf6070637336821f44557b16abfab307fc5880b1
tao.chai <tao.chai@thoughtworks.com>
2021-05-29 11:05:11 +0800

    [Tao & Yongqiang] throw exception when saving bag to full locker.

--- src/main/java/Locker.java
+++ src/main/java/Locker.java
@@ -1,13 +1,17 @@
 public class Locker {
-//    private final int capacity;
-//    private int availableCapacity;
-//
-//    public Locker(int capacity){
-//        this.capacity = capacity;
-//        this.availableCapacity = capacity;
-//    }
+    private final int capacity;
+    private int availableCapacity;
+
+    public Locker(int capacity) {
+        this.capacity = capacity;
+        this.availableCapacity = capacity;
+    }
 
     public Receipt save(Bag bag) {
+        if (availableCapacity <= 0) {
+            throw new RuntimeException("locker full");
+        }
+        availableCapacity--;
         return new Receipt();
     }
 }
--- src/test/java/LockerTest.java
+++ src/test/java/LockerTest.java
@@ -9,18 +9,18 @@ public class LockerTest {
     @Test
     void should_return_receipt_when_save_bag_given_available_locker() {
         Bag bag = new Bag();
-        Locker locker = new Locker();
+        Locker locker = new Locker(20);
         Receipt receipt = locker.save(bag);
-
         assertNotNull(receipt);
     }
 
     @Test
-    void should_throw_exception_when_save_bag_given_locker_full(){
+    void should_throw_exception_when_save_bag_given_locker_full() {
         Bag bag = new Bag();
-        Locker locker = new Locker();
+        Locker locker = new Locker(1);
+        locker.save(new Bag());
+        Exception exception = assertThrows(Exception.class, () -> locker.save(bag));
+        assertEquals("locker full", exception.getMessage());
 
-        assertThrows(Exception.class, () -> locker.save(bag));
-        assertEquals(new Exception("Locker Full"), locker.save(bag));
     }
 }
