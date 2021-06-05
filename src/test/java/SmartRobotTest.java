0879a431a1ee33cdb1b27a9d82273d18f22e156f
tao.chai <tao.chai@thoughtworks.com>
2021-06-05 10:22:26 +0800

    [Tao & Yang] add retrieve bag tests

--- src/test/java/SmartRobotTest.java
+++ src/test/java/SmartRobotTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SmartRobotTest {
 
     @Test
@@ -53,5 +56,42 @@ public class SmartRobotTest {
 
         Assertions.assertThrows(RuntimeException.class, () -> smartRobot.save(bag), "all lockers full");
 
+    }
+
+    @Test
+    void should_return_correct_bag_when_retrieving_bag_given_valid_receipt() {
+        Bag expectedBag = new Bag();
+        Locker firstLocker = new Locker(1);
+        Locker secondLocker = new Locker(1);
+
+        SmartRobot robot = new SmartRobot(Arrays.asList(firstLocker, secondLocker));
+        Receipt receipt = robot.save(expectedBag);
+
+        assertEquals(expectedBag, robot.retrieve(receipt));
+    }
+
+    @Test
+    void should_throw_exception_when_retrieving_given_fake_receipt() {
+        Bag expectedBag = new Bag();
+        Locker firstLocker = new Locker(1);
+        Locker secondLocker = new Locker(1);
+
+        Robot robot = new SmartRobot(Arrays.asList(firstLocker, secondLocker));
+        robot.save(expectedBag);
+
+        assertThrows(Exception.class, () -> robot.retrieve(new Receipt()), "invalid receipt");
+    }
+
+    @Test
+    void should_throw_exception_when_retrieving_given_used_receipt() {
+        Bag expectedBag = new Bag();
+        Locker firstLocker = new Locker(1);
+        Locker secondLocker = new Locker(1);
+
+        Robot robot = new SmartRobot(Arrays.asList(firstLocker, secondLocker));
+        Receipt receipt = robot.save(expectedBag);
+        robot.retrieve(receipt);
+
+        assertThrows(Exception.class, () -> robot.retrieve(receipt), "invalid receipt");
     }
 }
