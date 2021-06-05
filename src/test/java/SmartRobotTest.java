d0299ebdfe32493de1201644b44c2b670618c50d
tao.chai <tao.chai@thoughtworks.com>
2021-06-05 10:20:20 +0800

    [Tao & Yang] should throw exception from second locker when save bag given three lockers are full

--- src/test/java/SmartRobotTest.java
+++ src/test/java/SmartRobotTest.java
@@ -38,4 +38,20 @@ public class SmartRobotTest {
         Assertions.assertNotNull(receipt);
         Assertions.assertEquals(9, locker2.getAvailableCapacity());
     }
+
+    @Test
+    void should_throw_exception_when_save_bag_given_three_lockers_and_there_are_all_lockers_are_full() {
+        Bag bag = new Bag();
+        Locker locker1 = new Locker(1);
+        Locker locker2 = new Locker(1);
+        Locker locker3 = new Locker(1);
+        locker1.save(new Bag());
+        locker2.save(new Bag());
+        locker3.save(new Bag());
+
+        SmartRobot smartRobot = new SmartRobot(Arrays.asList(locker1, locker2, locker3));
+
+        Assertions.assertThrows(RuntimeException.class, () -> smartRobot.save(bag), "all lockers full");
+
+    }
 }
