a3105a4ec09eb8404b3ab8cb04467e8d08e76305
tao.chai <tao.chai@thoughtworks.com>
2021-06-05 10:16:27 +0800

    [Tao & Yang] should return_receipt from second locker when save bag given three lockers and two lockers have same max capacity

--- src/test/java/SmartRobotTest.java
+++ src/test/java/SmartRobotTest.java
@@ -23,5 +23,19 @@ public class SmartRobotTest {
 
     }
 
+    @Test
+    void should_return_receipt_from_second_locker_when_save_bag_given_three_lockers_and_there_are_two_lockers_have_same_max_capacity() {
+        Bag bag = new Bag();
+        Locker locker1 = new Locker(10);
+        Locker locker2 = new Locker(10);
+        Locker locker3 = new Locker(10);
+        locker1.save(new Bag());
 
+        SmartRobot smartRobot = new SmartRobot(Arrays.asList(locker1, locker2, locker3));
+
+        Receipt receipt = smartRobot.save(bag);
+
+        Assertions.assertNotNull(receipt);
+        Assertions.assertEquals(9, locker2.getAvailableCapacity());
+    }
 }
